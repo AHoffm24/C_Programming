4fd90f0b6d0c75518e9c74b50640c3f4d57c90ab
Adam Hoffman <ahoffm24@github.com>
2018-05-17 01:44:23 -0700

    Adding a program that will allow you to count your number of line feeds, character count(including line breaks as 1 for each break), and word count.

---
+++ wordCounting.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define IN 1 //inside a Word
+#define OUT 0 //outside a word;
+
+//count lines and characters in input
+main()
+{
+  printf("To stop counting type '~' into the command line\n");
+  int c, nl, nw, nc,state;
+  state = OUT;
+  nl = nw = nc = 0;
+  while ((c = getchar()) != '~'){
+    ++nc;
+
+    if(c == '\n')
+      ++nl;
+
+    if (c == ' ' || c == '\n' || c == '\t')
+      state = OUT;
+
+    else if (state == OUT){
+      state = IN;
+      ++nw;
+    }
+
+  }
+
+  printf("%d %d %d\n", nl, nw, nc);
+}
